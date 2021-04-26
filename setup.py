8f60e5cb66c5c829d24fcc90b2d23d742263c91d
Andrej <andrej.prek91@gmail.com>
2021-04-26 19:42:45 +0200

    folder structure changes

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='arpsls_game',
+setup(name='rpsls_game',
       version='1.0',
       description='game',
-      packages=['arpsls_game'],
+      packages=['rpsls_game'],
       author = 'Andrej Cop Prek',
       author_email = 'andrej.prek91@gmail.com',
       zip_safe=False)
